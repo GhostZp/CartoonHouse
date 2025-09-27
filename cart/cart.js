99e8dab3d6f92ee26784b21bcbbfb2f4b807c5ac
patrivar <patric.varis@metropolia.fi>
2025-09-27 22:49:33 +0300

    Working ShoppingCart

---
+++ cart/cart.js
@@ -0,0 +1,107 @@
+// Shoppin cart
+let cart = [];
+
+// Render products to main body
+function renderProducts() {
+    const productsContainer = document.querySelector('.products');
+    product.forEach(product => {
+    const productHTML = `
+        <div class="item">
+            <div class="item-container">
+                <div class="item-img">
+                    <img src="${product.image}" alt="${product.name}">
+                </div>
+                <div class="describe">
+                    <h2>${product.name}</h2>
+                    <p>${product.price} €</p>
+                </div>
+                <div class="add-to-cart" onclick="addToCart('${product.id}')">
+                    <img src="../src/img/cartoonhouse_green.png" alt="Add to cart">
+                </div>
+            </div>
+        </div>
+    `;
+    productsContainer.innerHTML += productHTML;
+    });
+}
+
+// Add products to shopping cart
+function addToCart(id) {
+    if (cart.some(item => item.id === id)) {
+        console.log('Tuote on jo korissa');
+        changeUnitAmount('plus', id)
+    } else {
+        const item = product.find(p => p.id === id);
+        cart.push({ ...item, units: 1 });
+        console.log('Lisätty koriin:', cart);
+    }
+    updateCart();
+};
+
+// Render items to cart
+function updateCart() {
+    renderCartItems();
+    renderTotal();
+};
+
+function renderCartItems() {
+    const cartItems = document.querySelector('.cart-items');
+    cartItems.innerHTML = "";  // Clear cart
+    cart.forEach((item) => {
+        cartItems.innerHTML += `
+            <div class="cart-item">
+                <div class="item-img">
+                    <img src="${item.image}" alt="${item.name}">
+                </div>
+                <div>
+                    <h4>${item.name}</h4>
+                </div>
+                <div class="price-info">
+                    <small>${item.price}€</small>
+                </div>
+                <div class="unit-count">
+                    <span class="button-plus" onclick="changeUnitAmount('plus', '${item.id}')">+</span>
+                    <span class="unit-counter">${item.units}</span>
+                    <span class="button-minus" onclick="changeUnitAmount('minus', '${item.id}')">-</span>
+                </div>
+            </div>
+        `
+    });
+};
+
+// Change the amount of single item
+function changeUnitAmount(action, id) {
+    cart = cart.map((item) => {
+        let units = item.units;
+
+        if (item.id === id) {
+            if (action === 'minus') {
+                if (units === 1){
+                    return null; // Remove item from cart
+                }else{
+                    units --;
+                }
+            } else if (action === 'plus'){
+                units ++;
+            }
+            return {
+                ...item,
+                units,
+            };
+        }
+        return item;
+    }).filter(Boolean);
+    updateCart();
+};
+
+
+// Calculate and render total price
+function renderTotal() {
+    const subTotal = document.querySelector('.total-price');
+    let totalPrice = 0;
+
+    cart.forEach((item) => {
+        totalPrice += item.price * item.units;
+    })
+    subTotal.innerHTML = `${totalPrice} €`;
+};
