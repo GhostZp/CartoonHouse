fad616b30aece857313587759ecfb7df6cb2fd04
patrivar <patric.varis@metropolia.fi>
2025-10-29 10:14:35 +0200

    HeaderJaFooter+CartProductNimenKorjauksia

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,15 +3,15 @@ let cart = [];
 
 // Render items to cart
 function updateCart() {
-    renderCartItems();
-    renderTotal();
-};
+  renderCartItems();
+  renderTotal();
+}
 
 function renderCartItems() {
-    const cartItems = document.querySelector('.cart-items');
-    cartItems.innerHTML = "";  // Clear cart
-    cart.forEach((item) => {
-        cartItems.innerHTML += `
+  const cartItems = document.querySelector(".cart-items");
+  cartItems.innerHTML = ""; // Clear cart
+  cart.forEach((item) => {
+    cartItems.innerHTML += `
             <div class="cart-item">
                 <div class="cart-item-img">
                     <img src="${item.image}" alt="${item.name}">
@@ -28,43 +28,44 @@ function renderCartItems() {
                     <span class="button-minus" onclick="changeUnitAmount('minus', '${item.id}')">-</span>
                 </div>
             </div>
-        `
-    });
-};
+        `;
+  });
+}
 
 // Change the amount of single item
 function changeUnitAmount(action, id) {
-    cart = cart.map((item) => {
-        let units = item.units;
+  cart = cart
+    .map((item) => {
+      let units = item.units;
 
-        if (item.id === id) {
-            if (action === 'minus') {
-                if (units === 1){
-                    return null; // Remove item from cart
-                }else{
-                    units --;
-                }
-            } else if (action === 'plus'){
-                units ++;
-            }
-            return {
-                ...item,
-                units,
-            };
+      if (item.id === id) {
+        if (action === "minus") {
+          if (units === 1) {
+            return null; // Remove item from cart
+          } else {
+            units--;
+          }
+        } else if (action === "plus") {
+          units++;
         }
-        return item;
-    }).filter(Boolean);
-    updateCart();
-};
-
+        return {
+          ...item,
+          units,
+        };
+      }
+      return item;
+    })
+    .filter(Boolean);
+  updateCart();
+}
 
 // Calculate and render total price
 function renderTotal() {
-    const subTotal = document.querySelector('.total-price');
-    let totalPrice = 0;
+  const subTotal = document.querySelector(".total-price");
+  let totalPrice = 0;
 
-    cart.forEach((item) => {
-        totalPrice += item.price * item.units;
-    })
-    subTotal.innerHTML = `${totalPrice} €`;
-};
+  cart.forEach((item) => {
+    totalPrice += item.price * item.units;
+  });
+  subTotal.innerHTML = `${totalPrice} €`;
+}
