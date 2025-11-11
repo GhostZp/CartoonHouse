077451db3436f498404c3e972e312438626f5395
patrivar <patric.varis@metropolia.fi>
2025-11-11 19:03:54 +0200

    Cart muutettu dialogiksi ja lisätty yhden painikkeen cartin tyhjennys

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,25 +1,37 @@
 // DOM-Listener
-document.addEventListener('DOMContentLoaded', () => {
-    renderProducts(appetizers, 'Appetizers');
-    renderProducts(main_dishes, 'Main_dishes');
-    renderProducts(desserts, 'Desserts');
-    renderProducts(drinks, 'Drinks');
+document.addEventListener("DOMContentLoaded", () => {
+  renderProducts(appetizers, "Appetizers");
+  renderProducts(main_dishes, "Main_dishes");
+  renderProducts(desserts, "Desserts");
+  renderProducts(drinks, "Drinks");
 
-    const showCartButton = document.querySelector('.cart-button');
-    const closeCartButton = document.querySelector('.close-button');
-    const body = document.querySelector('body');
+  //cartDialog and its opening and closing functions
+  const cartModal = document.querySelector("#cart");
+  const openModal = document.querySelector(".cart-button");
+  const closeModal = document.querySelector(".close-button");
 
-    showCartButton.addEventListener('click', () => {
-        body.classList.add('show-cart');
-    });
+  openModal.addEventListener("click", () => {
+    cartModal.classList.remove("slide-out");
+    cartModal.classList.add("slide-in");
+    cartModal.show();
+  });
 
-    closeCartButton.addEventListener('click', () => {
-        body.classList.remove('show-cart');
-    });
+  closeModal.addEventListener("click", () => {
+    cartModal.classList.remove("slide-in");
+    cartModal.classList.add("slide-out");
+  });
+
+  // Empty your cart
+  document.getElementById("trash").addEventListener("click", () => {
+    const cartItems = document.querySelector(".cart-items");
+    const subTotal = document.querySelector(".total-price");
+    cartItems.innerHTML = ""; // Clear cart
+    subTotal.innerHTML = "0 €";
+  });
 });
 
 // Opens Appetizers tab when site loads
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const firstTab = document.getElementsByClassName("tablinks")[0];
   openMenu({ currentTarget: firstTab }, "Appetizers");
-})
+});
